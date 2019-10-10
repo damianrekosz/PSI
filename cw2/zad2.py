b1b17a7e70b43f6e7ce4c75b72a7c0221d9570db
Cezary Dobreńko <bonzohaker742@gmail.com>
2019-10-10 10:22:14 +0200

    cwiczenia

---
+++ cw2/zad2.py
@@ -0,0 +1,8 @@
+imie = "Damian"
+nazwisko = "Rekosz"
+litera_1 = imie[2]
+litera_2 = nazwisko[3]
+liczba_liter1 = "W tekście jest liter oraz liter".count(litera_1)
+liczba_liter2 = "W tekście jest liter oraz liter".count(litera_2)
+print("W tekście jest " + str(liczba_liter1) + " liter " + litera_1 + " oraz " + str(liczba_liter2) + " liter " + litera_2 + "\n")
+
