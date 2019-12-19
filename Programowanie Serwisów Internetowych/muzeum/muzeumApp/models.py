121865c87badafcf30655258d23cd919958544d3
Patriotpl <patriotpl22@gmail.com>
2019-12-19 11:17:41 +0100

    działa autoryzacja :3

--- Programowanie Serwisów Internetowych/muzeum/muzeumApp/models.py
+++ Programowanie Serwisów Internetowych/muzeum/muzeumApp/models.py
@@ -25,4 +25,3 @@ class Wydarzenia(models.Model):
     sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
     nazwa = models.CharField(max_length=200)
     wlasciciel = models.ForeignKey('auth.User', related_name='wydarzenia', on_delete=models.CASCADE)
-    #hehe
