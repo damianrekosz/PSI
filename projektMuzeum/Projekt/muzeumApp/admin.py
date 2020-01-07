6a2158c81ffd2874dda74c3feb8b9af41bc9504b
damianrekosz <damianrekosz@live.com>
2020-01-07 16:41:16 +0100

    projekt

--- projektMuzeum/Projekt/muzeumApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Pracownicy, Eksponaty, Sale, Wydarzenia
-
-admin.site.register(Pracownicy)
-admin.site.register(Eksponaty)
-admin.site.register(Sale)
-admin.site.register(Wydarzenia)
-
-# 12345678
