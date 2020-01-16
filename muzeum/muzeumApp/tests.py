24133b752b3f2ce4529e60ee7c95d2f4a8f42edb
Patriotpl <patriotpl22@gmail.com>
2020-01-16 10:58:36 +0100

    aha

--- muzeum/muzeumApp/tests.py
+++ muzeum/muzeumApp/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
-
-# Create your tests here.
+#import pytest
+#from muzeum.muzeumApp.models import Pracownicy
+#from django.urls import reverse
+#
+#
+#def test_add_prac(authorized_client):
+#    data = {
+#        'nazwisko' : 'Janowicz',
+#        'imie' : 'Jan',
+#        'pesel' : '34985768556',
+#    }
+#
+#    url = reverse('muzeumapp:pracownicy')
+#
+#    response = authorized_client.post(
+#        url,
+#        data,
+#        content_type = 'application/json',
+#    )
+#
+#    assert response.status_code == 200
+#    assert response.json() == data
+#    all_pracownicy = Pracownicy.objects.all()
+#    assert all_pracownicy.count() == 1
+#    saved_pracownik = all_pracownicy.first()
+#    assert saved_pracownik.nazwisko == 'Janowicz'
+#    assert saved_pracownik.imie == 'Jan'
+#    assert saved_pracownik.pesel == '34985768556'
